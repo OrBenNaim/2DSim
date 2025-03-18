871066ea13dfc63e59a87db5d030dde458494009
Or Ben Naim <orbennaim123@gmail.com>
2025-03-18 15:10:27 +0200

    .

--- gol_basic_simulation.py
+++ gol_basic_simulation.py
@@ -2,9 +2,9 @@ import numpy as np
 import time
 import os
 
-def game_of_life(filename, generations, ):
+def game_of_life(filename, generations):
 
 
 
 if __name__ == "__main__":
-    game_of_life("pattern.txt", steps=50, delay=0.2)
+    game_of_life("pattern.txt", steps=50)
