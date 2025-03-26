6827a02112b1d518e6b32a696928acb5edfc47c9
Or Ben Naim <orbennaim123@gmail.com>
2025-03-26 17:16:14 +0200

    Docstring added to get_config

--- src/utils.py
+++ src/utils.py
@@ -8,4 +8,16 @@ with open(FOLDER_CONFIG_PATH, "r", encoding="utf-8") as file:
 
 # Function to access config
 def get_config():
+    """
+    Retrieves the loaded configuration data.
+
+    This function provides access to the global configuration settings
+    that were loaded from the YAML file at the start of the program. It
+    returns the configuration data as a Python dictionary, allowing other
+    parts of the program to access the configuration without reloading
+    the YAML file repeatedly.
+
+    Returns:
+        dict: The configuration data loaded from the YAML file.
+    """
     return CONFIG_DATA
